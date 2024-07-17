0584385c4a6345f92cf53b9c06ec4fe6506925dd
iamowaisazam <iamowaisazam@gmail.com>
2024-07-18 01:36:14 +0500

    update

---
+++ docs/chunk-SXISWLJU.js
@@ -0,0 +1,1 @@
+import{Jb as a,Nb as r,ga as i,qb as o,rb as s}from"./chunk-6CRNKGI6.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-settings"]],standalone:!0,features:[r],decls:2,vars:0,template:function(n,c){n&1&&(o(0,"p"),a(1,"settings works!"),s())}});let t=e;return t})();export{d as SettingsComponent};
